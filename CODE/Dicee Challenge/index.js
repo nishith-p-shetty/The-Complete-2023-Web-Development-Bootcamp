5e34b6e317249143c8408bcd69711113a5e53142
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-02-10 01:02:54 +0530

    .

---
+++ CODE/Dicee Challenge/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = ( Math.floor( Math.random() * 6 ) + 1 );
+var randomNumber2 = ( Math.floor( Math.random() * 6 ) + 1 );
+
+document.querySelector(".img1").setAttribute("src", ("images/dice" + randomNumber1 +".png") );
+document.querySelector(".img2").setAttribute("src", ("images/dice" + randomNumber2 +".png") );
+
+var h1 = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2)
+{
+    h1.textContent = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1)
+{
+    h1.textContent = "Player 2 Wins! 🚩";
+}
+else
+{
+    h1.textContent = "Draw!";
+}
