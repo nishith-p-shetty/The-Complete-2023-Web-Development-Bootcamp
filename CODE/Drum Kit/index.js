82f1f1c15e600c474f85029e08f072b59650eba5
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-02-10 02:01:23 +0530

    .

---
+++ CODE/Drum Kit/index.js
@@ -0,0 +1,70 @@
+// function handelclick()
+// {
+//     alert("a");
+// }
+
+
+// var w = document.querySelectorAll("button")[0].addEventListener("click", handelclick);
+// var a = document.querySelectorAll("button")[1].addEventListener("click", handelclick);
+// var s = document.querySelectorAll("button")[2].addEventListener("click", handelclick);
+// var d = document.querySelectorAll("button")[3].addEventListener("click", handelclick);
+// var j = document.querySelectorAll("button")[4].addEventListener("click", handelclick);
+// var k = document.querySelectorAll("button")[5].addEventListener("click", handelclick);
+// var l = document.querySelectorAll("button")[6].addEventListener("click", handelclick);
+
+for (var i = 0 ; i < (document.querySelectorAll(".drum").length) ; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        soundOP(this.textContent);
+    });
+}
+
+document.addEventListener("keydown", function (event)
+{
+    soundOP(event["key"]);
+});
+
+
+function soundOP(key)
+{
+    switch (key)
+    {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+            
+        case "k":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+                
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        
+        default:
+            break;
+    }
+}
