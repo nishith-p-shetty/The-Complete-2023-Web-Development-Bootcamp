029d3b85968ede5adf416af7472fcee88cbbf771
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-02-10 00:19:57 +0530

    .

--- CODE/DOM-Challenge/index.js
+++ CODE/DOM-Challenge/index.js
@@ -1,1 +1,21 @@
-document.query
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function btnclick() {
+    document.querySelector("input").click();
+}
+
+
+var name1 = prompt("Enter your name");
+alert("Hello " + name1);
+document.querySelector("h1").innerHTML = ("Hello " + name1);
+btnclick();
+sleep(2000).then(() => { 
+    btnclick();
+    document.querySelector("ul").lastElementChild.innerHTML = "abc";
+});
+
+document.getElementsByTagName("li")[2].style.color = "purple"
+
+document.querySelector("li a").style.color = "red";
