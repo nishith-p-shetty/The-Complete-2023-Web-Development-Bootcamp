7fb954eec24d9df3a2c42c9b7c184ff586e2db24
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-02-24 17:39:03 +0530

    .

--- CODE/NewsLetter-Signup/app.js
+++ CODE/NewsLetter-Signup/app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/signup.html');
 });
 
+app.post('/',  (req, res) => {
+    res.send(req.body);
+});
+
 app.listen(port, () => {
     console.log("Server started on port " + port);
 });
