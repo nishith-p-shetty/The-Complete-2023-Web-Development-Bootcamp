3e613991fa641dc40de5effd71b70923f18517a1
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-02-24 17:20:57 +0530

    .

---
+++ CODE/NewsLetter-Signup/app.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const request = require('request');
+const express = require('express');
+const app = express();
+const port =  3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.listen(port, () => {
+    console.log("Server started on port " + port);
+});
