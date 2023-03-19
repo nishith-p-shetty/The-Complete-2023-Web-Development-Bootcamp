8124050e0874e54a8d57d193199c95ba9f6b6a40
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-03-19 18:58:51 +0530

    .

---
+++ CODE/ToDo-List-V1/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app =  express();
+
+app.set('view engine', 'ejs');
+
+var options = {
+    weekday: 'long',
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric',
+};
+
+app.get('/day', (req, res)=> {
+    var today = new Date();
+    var cd = '', work = '';
+    var daystr = today.toLocaleDateString('en-US', options);
+    if (today.getDay() === 0 || today.getDay() === 6)
+    {
+        cd = 'Weekend';
+        work = 'Enjoy';
+    }
+    else
+    {
+        cd = 'Weekday';
+        work = 'Work';
+    }
+    res.render('list', {
+        daytype: cd,
+        daywork: work,
+        dayno: today.getDay(),
+        dystr: daystr,
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('/');
+})
+
+
+app.listen(8000, () => {
+    console.log('server started on port 8000');
+});
