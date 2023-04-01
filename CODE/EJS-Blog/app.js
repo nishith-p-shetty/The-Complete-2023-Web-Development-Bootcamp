db5610f4d9ea061ef5b691b192b4c69064f3a553
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-04-02 00:37:43 +0530

    ejs

--- CODE/EJS-Blog/app.js
+++ CODE/EJS-Blog/app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,18 +14,57 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//let posts = [{title: "Home", content: homeStartingContent}];
+let posts = [
+  {
+    title: 'Home',
+    content: 'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.'
+  },
+  {
+    title: 'asdf',
+    content: 'adfadsf nksdnajmb jnnsjan nfs fnd nfd nfkd fnnf aknfjka nfkan fdk afndksfnakd fnknfaknfanfkand landf a,nfanfad nfnf nak nakfnk fnakdfnkafn aknadfadsf nksdnajmb jnnsjan nfs fnd nfd nfkd fnnf aknfjka nfkan fdk afndksfnakd fnknfaknfanfkand landf a,nfanfad nfnf nak nakfnk fnakdfnkafn aknadfadsf nksdnajmb jnnsjan nfs fnd nfd nfkd fnnf aknfjka nfkan fdk afndksfnakd fnknfaknfanfkand landf a,nfanfad nfnf nak nakfnk fnakdfnkafn aknadfadsf nksdnajmb jnnsjan nfs fnd nfd nfkd fnnf aknfjka nfkan fdk afndksfnakd fnknfaknfanfkand landf a,nfanfad nfnf nak nakfnk fnakdfnkafn akn'
+  },
+  {
+    title: 'USHDjnjdn',
+    content: 'njakhn ibdkjadl;a jnakdfasd.fas.dfadfh s;fasdfasd,fas.dfsd.af asdfnaskdfd. dfsd.f .sdf lasd.fma.sdf ds.f a.sdf mnjakhn ibdkjadl;a jnakdfasd.fas.dfadfh s;fasdfasd,fas.dfsd.af asdfnaskdfd. dfsd.f .sdf lasd.fma.sdf ds.f a.sdf mnjakhn ibdkjadl;a jnakdfasd.fas.dfadfh s;fasdfasd,fas.dfsd.af asdfnaskdfd. dfsd.f .sdf lasd.fma.sdf ds.f a.sdf mnjakhn ibdkjadl;a jnakdfasd.fas.dfadfh s;fasdfasd,fas.dfsd.af asdfnaskdfd. dfsd.f .sdf lasd.fma.sdf ds.f a.sdf mnjakhn ibdkjadl;a jnakdfasd.fas.dfadfh s;fasdfasd,fas.dfsd.af asdfnaskdfd. dfsd.f .sdf lasd.fma.sdf ds.f a.sdf m'
+  },
+  {
+    title: 'Home',
+    content: 'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.'
+  },
+]
 
+app.get( '/', (req, res) => {
+  res.render('home', {posts: posts});
+});
 
+app.get( '/posts/:title', (req, res) => {
+  const a = _.lowerCase(req.params.title);
+  posts.forEach(function(post) {
+    const b = _.lowerCase(post.title);
+    if(a === b){
+      res.render('post', {title: post.title, content: post.content});
+    }
+  });
+  res.redirect('/');
+});
 
+app.get( '/about', (req, res) => {
+  res.render('about', {aboutContent: aboutContent});
+});
 
+app.get( '/contact', (req, res) => {
+  res.render('contact', {contactContent: contactContent});
+});
 
+app.get( '/compose', (req, res) => {
+  res.render('compose');
+});
 
-
-
-
-
-
-
+app.post( '/compose', (req, res) => {
+  posts.push({title: req.body.postTitle, content: req.body.postBody});
+  res.redirect('/');
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
