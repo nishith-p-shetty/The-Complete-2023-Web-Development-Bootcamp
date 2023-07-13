b6a6dcebe7a67fa764bdad562977316445b4ffac
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-07-13 19:45:35 +0530

    .

--- CODE/Keeper App/src/index.js
+++ CODE/Keeper App/src/index.js
@@ -9,7 +9,7 @@
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+//HINT: You will need to study the classes in teh styles.css file to apply styling.
 
 import React from 'react';
 import ReactDom from 'react-dom';
