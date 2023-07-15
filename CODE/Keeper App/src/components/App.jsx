977875a239e1dfb1ec537f80d945f07fc22f0d9c
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-07-15 18:28:26 +0530

    notes app

--- CODE/Keeper App/src/components/App.jsx
+++ CODE/Keeper App/src/components/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
+import CreateArea from './CreateArea';
 
-import notes from '../notes';
+import notesfixed from '../notes';
 
 function App() {
+
+    const [notes, insertNotes] = useState([]);
+
+    function addNote(newNote) {
+        insertNotes((prevNotes) => {
+            return ([...prevNotes, newNote]);
+        });
+    }
+
+    function deleteNote(id) {
+        insertNotes((prevNotes) => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        });
+    }
+
     return (
         <>
             <Header />
-            {notes.map(x => <Note key={x.key} title={x.title} content={x.content}/>)}
+            <CreateArea onAdd={addNote} />
+            {notes.map((nt, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={nt.title}
+                        content={nt.content}
+                        onDelete={deleteNote}
+                    />);
+            })}
+            {/*notesfixed.map(x => <Note key={x.key} title={x.title} content={x.content}/>)*/}
             <Footer />
         </>
     );
